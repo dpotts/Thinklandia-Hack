1fe2ebbf7e17f8faaf4aec702871a5544547bd99
Daniel Potts <dan.t.potts@gmail.com>
2013-09-13 16:34:24 -0700

    Hope Renewed

---
+++ Thinklandia-Hack/FullSkeletonSKD1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Kinect;
+
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Media;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ANewHope
+{
+    public partial class Form1 : Form
+    {
+        KinectSensor sensor;
+        public Form1()
+        {
+            InitializeComponent();
+            sensor = KinectSensor.KinectSensors[0];
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+            sensor.SkeletonStream.Enable();
+
+            sensor.AllFramesReady += FramesReady;
+            sensor.Start();
+        }
+
+        private static Bitmap ExtractBodyPartBitmap(KinectSensor sensor,Skeleton skeleton, Graphics g, JointType jointType,float widthMeters,float heightMeters)
+        {
+            ColorImageStream stream = sensor.ColorStream;
+            ColorImageFormat imageFormat = stream.Format;
+            int stride = stream.FrameWidth * stream.FrameBytesPerPixel;
+            CoordinateMapper map = new CoordinateMapper(sensor);
+
+            // Get the position of the joint
+            SkeletonPoint skeletonPoint = skeleton.Joints[jointType].Position;
+
+            // Map joint position to pixel coordinates
+            ColorImagePoint colorPoint = map.MapSkeletonPointToColorPoint(skeletonPoint, imageFormat);
+
+            // Estimate a cutout rectangle centered on the pixel coordinates
+            //Int32Rect rect = EstimateCutoutRect(stream, colorPoint, skeletonPoint.Z, widthMeters, heightMeters);
+            Bitmap bitmap = DrawHead(stream, g, colorPoint, skeletonPoint.Z, widthMeters, heightMeters);
+
+            // Cut out the appropriate part of the image
+            //WriteableBitmap bitmap = new WriteableBitmap(rect.Width, rect.Height, 96, 96, PixelFormats.Bgr32, null);
+            //bitmap.WritePixels(rect, imageData, stride, 0, 0);
+            return bitmap;
+        }
+
+        private static Bitmap DrawHead(ColorImageStream stream, Graphics g, ColorImagePoint colorPoint, float depthMeters, float widthMeters, float heightMeters)
+        {
+            float focalLength = stream.NominalFocalLengthInPixels;
+            int widthPixels = (int)((widthMeters * focalLength) / depthMeters);
+            int heightPixels = (int)((heightMeters * focalLength) / depthMeters);
+
+            int x = System.Math.Max(0, colorPoint.X - (widthPixels / 2));
+            int y = System.Math.Max(0, colorPoint.Y - (heightPixels / 2));
+            widthPixels = System.Math.Min(widthPixels, stream.FrameWidth - x);
+            heightPixels = System.Math.Min(heightPixels, stream.FrameHeight - y);
+
+            Pen skyBluePen = new Pen(Brushes.White);
+
+            // Set the pen's width.
+            skyBluePen.Width = 5.0F;
+
+            // Set the LineJoin property.
+            skyBluePen.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+
+            Bitmap bitmap = new Bitmap(widthPixels, heightPixels, PixelFormat.Format32bppRgb);
+
+            //g = Graphics.FromImage(bitmap);
+
+
+
+            g.DrawEllipse(skyBluePen, x , (y + 15.0f), widthPixels, heightPixels);
+
+            return bitmap;
+        }
+
+        /*private static Int32Rect EstimateCutoutRect(ColorImageStream stream,ColorImagePoint colorPoint,float depthMeters,float widthMeters,float heightMeters)
+        {
+            // Convert dimensions in meters to pixels, taking depth distance into account
+            float focalLength = stream.NominalFocalLengthInPixels;
+            int widthPixels = (int)((widthMeters * focalLength) / depthMeters);
+            int heightPixels = (int)((heightMeters * focalLength) / depthMeters);
+
+            // Calculate rectangle, taking care to stay in-bounds
+            int x = System.Math.Max(0, colorPoint.X - (widthPixels / 2));
+            int y = System.Math.Max(0, colorPoint.Y - (heightPixels / 2));
+            widthPixels = System.Math.Min(widthPixels, stream.FrameWidth - x);
+            heightPixels = System.Math.Min(heightPixels, stream.FrameHeight - y);
+            return new Int32Rect(x, y, widthPixels, heightPixels);
+        }*/
+
+        void FramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            ColorImageFrame VFrame = e.OpenColorImageFrame();
+            if (VFrame == null) return;
+            byte[] pixelS = new byte[VFrame.PixelDataLength];
+            Bitmap bmap = ImageToBitmap(VFrame);
+
+
+            SkeletonFrame SFrame = e.OpenSkeletonFrame();
+            if (SFrame == null) return;
+
+            Graphics g = Graphics.FromImage(bmap);
+            Skeleton[] Skeletons = new Skeleton[SFrame.SkeletonArrayLength];
+            SFrame.CopySkeletonDataTo(Skeletons);
+
+            foreach (Skeleton S in Skeletons)
+            {
+                if (S.TrackingState == SkeletonTrackingState.Tracked)
+                {
+
+                    /*//body
+                    DrawBone(JointType.Head, JointType.ShoulderCenter, S, g);
+                    DrawBone(JointType.ShoulderCenter, JointType.Spine, S, g);
+                    DrawBone(JointType.Spine, JointType.HipCenter, S, g);
+                    //left leg
+                    DrawBone(JointType.HipCenter, JointType.HipLeft, S, g);
+                    DrawBone(JointType.HipLeft, JointType.KneeLeft, S, g);
+                    DrawBone(JointType.KneeLeft, JointType.AnkleLeft, S, g);
+                    DrawBone(JointType.AnkleLeft, JointType.FootLeft, S, g);
+                    //Right Leg
+                    DrawBone(JointType.HipCenter, JointType.HipRight, S, g);
+                    DrawBone(JointType.HipRight, JointType.KneeRight, S, g);
+                    DrawBone(JointType.KneeRight, JointType.AnkleRight, S, g);
+                    DrawBone(JointType.AnkleRight, JointType.FootRight, S, g);
+                    //Left Arm
+                    DrawBone(JointType.ShoulderCenter, JointType.ShoulderLeft, S, g);
+                    DrawBone(JointType.ShoulderLeft, JointType.ElbowLeft, S, g);
+                    DrawBone(JointType.ElbowLeft, JointType.WristLeft, S, g);
+                    DrawBone(JointType.WristLeft, JointType.HandLeft, S, g);
+                    //Right Arm
+                    DrawBone(JointType.ShoulderCenter, JointType.ShoulderRight, S, g);
+                    DrawBone(JointType.ShoulderRight, JointType.ElbowRight, S, g);
+                    DrawBone(JointType.ElbowRight, JointType.WristRight, S, g);
+                    DrawBone(JointType.WristRight, JointType.HandRight, S, g);*/
+
+                    ExtractBodyPartBitmap(this.sensor, S, g, JointType.Head, 0.2f, 0.27f);
+
+
+                }
+
+
+
+            }
+            pictureBox1.Image = bmap;
+        }
+
+        void DrawBone(JointType j1, JointType j2, Skeleton S, Graphics g)
+        {
+            Point p1 = GetJoint(j1, S);
+            Point p2 = GetJoint(j2, S);
+            g.DrawLine(Pens.Red, p1, p2);
+        }
+
+        Point GetJoint(JointType j, Skeleton S)
+        {
+            SkeletonPoint Sloc = S.Joints[j].Position;
+            ColorImagePoint Cloc = sensor.CoordinateMapper.MapSkeletonPointToColorPoint(Sloc, ColorImageFormat.RawBayerResolution640x480Fps30);
+
+            return new Point(Cloc.X, Cloc.Y);
+        }
+
+
+
+        Bitmap ImageToBitmap(
+                           ColorImageFrame Image)
+        {
+            byte[] pixelData =
+                     new byte[Image.PixelDataLength];
+            Image.CopyPixelDataTo(pixelData);
+            Bitmap bmap = new Bitmap(
+                   Image.Width,
+                   Image.Height,
+                   PixelFormat.Format32bppRgb);
+            BitmapData bmapS = bmap.LockBits(
+              new Rectangle(0, 0,
+                         Image.Width, Image.Height),
+              ImageLockMode.WriteOnly,
+              bmap.PixelFormat);
+            IntPtr ptr = bmapS.Scan0;
+            Marshal.Copy(pixelData, 0, ptr,
+                       Image.PixelDataLength);
+            bmap.UnlockBits(bmapS);
+            return bmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sensor.Stop();
+        }
+
+
+
+
+    }
+}
