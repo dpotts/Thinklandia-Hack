529a3482238cef36240b361991d4d2a52f151427
Noel Feliciano <feliciano.noel@gmail.com>
2013-09-13 17:57:30 -0700

    oops! put the using in the right place this time

--- ThinklandiaHack/ThinklandiaHack/Properties/AssemblyInfo.cs
+++ ThinklandiaHack/ThinklandiaHack/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
-using Microsoft.Kinect;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
